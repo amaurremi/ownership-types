fa5969df8d0cf50ea7835934fd8a410be03279df
Marianna Rapoport <mrapoport@uwaterloo.ca>
2015-12-01 21:53:24 -0500

    more progress, compiles

--- src/TypeCheck.hs
+++ src/TypeCheck.hs
@@ -8,9 +8,9 @@ import AstTypes
 -- method signature type: return type + parameter types
 type Sig = ([OwnershipType], OwnershipType)
 
-type VarDict = Map.Map Name OwnershipType
+type VarDict = Map.Map VarName OwnershipType
 type FieldDict = Map.Map Name OwnershipType
-type MethodDict = Map.Map Name (Sig, [Name], Expr, VarDict)
+type MethodDict = Map.Map Name (Sig, [VarName], Expr, VarDict)
 
 type P = Prog
 type Σ = Set.Set Context
@@ -28,19 +28,18 @@ methodDict (Defn _ _ _ methods) = Map.fromList $
         in (n, ((argTypes, t), argNames, e, varDict args))
     ) methods
 
-varDictList :: [VarDec] -> [(Name, OwnershipType)]
-varDictList = map (\(VarDec vt vn) -> (vn, vt))
+varDictList :: [VarDec] -> [(VarName, OwnershipType)]
+varDictList = map (\(VarDec vt vn) -> (VarName vn, vt))
 
 varDict :: [VarDec] -> VarDict
 varDict = Map.fromList . varDictList
 
 typeCheck :: Prog -> Bool
 typeCheck prog@(Prog defns varDecs expr) =
-    let varsAndTypes         = map (\(VarDec t n) -> (n, t)) varDecs
-        (varNames, varTypes) = unzip varsAndTypes
+    let (varNames, varTypes) = unzip $ varDictList varDecs
     in (all (checkClass prog) defns) &&
        (all (checkType prog Set.empty) varTypes) &&
-       (checkExpr prog Set.empty (Map.fromList $ map (\(v, t) -> (VarName v, t)) varsAndTypes) expr)
+       (checkExpr prog Set.empty (varDict varDecs) expr)
 
 checkClass :: P -> Defn -> Bool
 checkClass prog defn@(Defn n cs fs ms) =
@@ -59,4 +58,10 @@ checkExpr :: P -> Σ -> Γ -> Expr -> Bool
 checkExpr prog sigma gamma e = error ""
 
 checkMethod :: P -> Σ -> Γ -> Method -> Bool
-checkMethod prog sigma gamma (Method t n args vars e) = error ""
+checkMethod prog sigma gamma (Method t n args vars e) =
+    let types    = snd . unzip . varDictList
+        argTypes = types args
+        varTypes = types vars
+        newGamma = gamma `Map.union` (varDict args) `Map.union` (varDict vars)
+    in (all (checkType prog sigma) $ t : argTypes ++ varTypes) &&
+       (checkExpr prog sigma newGamma e)
